5708910d33c974ef60224a971ef023b55b59ff76
QwertTheWert <fredrickwill2005@gmail.com>
2025-03-06 12:32:49 +0700

    turn to Flask

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(200), nullable=False)
+	password = db.Column(db.String(200), nullable=False)
+ 
+	def __repr__(self):
+		return '<User %r>' % self.id
+
+@app.route('/')
+def index():
+	return render_template("index.html")
+
+if __name__ == "__main__":
+	app.run(debug=True)
