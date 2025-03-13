d969e0dfd770a9e88351293036c9ddc955305e59
QwertTheWert <fredrickwill2005@gmail.com>
2025-03-13 11:19:34 +0700

    can register

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -7,9 +7,11 @@ db = SQLAlchemy(app)
 
 class User(db.Model):
 	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-	name = db.Column(db.String(200), nullable=False)
-	password = db.Column(db.String(200), nullable=False)
 	email = db.Column(db.String(200), nullable=False)
+	full_name = db.Column(db.String(200), nullable=False)
+	username = db.Column(db.String(200), nullable=False)
+	password = db.Column(db.String(200), nullable=False)
+	bank_number = db.Column(db.String(200), nullable=False)
 		
 	def __repr__(self):
 		return '<User %r>' % self.id
@@ -23,21 +25,18 @@ class ProductCategory(db.Model):
 
 class Product(db.Model):
 	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-	category_id = db.Column(db.Integer, nullable=False)
+	category_id = db.Column(db.Integer, db.ForeignKey('product_category.id'), nullable=False)
 	name = db.Column(db.String(200), nullable=False)
-	
-	category = db.relationship('ProductCategory', foreign_keys = "Product.category_id")
 
 	def __repr__(self):
 		return '<Product %r>' % self.id
 
 class ProductModel(db.Model):
 	id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-	product_id = db.Column(db.Integer, nullable=False)
+	product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
 	name = db.Column(db.String(200), nullable=False)
 	price = db.Column(db.Integer, nullable=False)
 	quantity = db.Column(db.Integer, nullable=False)
-	product = db.relationship('Product', foreign_keys = "ProductModel.product_id")
 
 	def __repr__(self):
 		return '<ProductModel %r>' % self.id
@@ -55,6 +54,26 @@ def register():
 @app.route('/login')
 def login():
 	return render_template("login.html")
+
+@app.route('/register', methods=["POST"])
+def on_register():
+	
+	full_name = request.form.get("full_name")
+	username = request.form.get("username")
+	email = request.form.get("email")
+	password = request.form.get("password")
+	bank_number = request.form.get("bank_number")
+	print(full_name, username, email, password, bank_number)
+	# return redirect('/register')
+	# create an object of the Profile class of models
+	# and store data as a row in our datatable
+	if full_name != "" and username != "" and email != "" and password != "" and bank_number != "":
+		new_user = User(full_name=full_name, username=username, email=email, password=password, bank_number=bank_number)
+		db.session.add(new_user)
+		db.session.commit()
+		return redirect('/')
+	else:
+		return redirect('/register')
 
 if __name__ == "__main__":
 	app.run(debug=True)
