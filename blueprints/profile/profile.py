78a5d643ce20f5b37e9837b49db64dc11edad849
QwertTheWert <fredrickwill2005@gmail.com>
2025-03-20 09:59:04 +0700

    added login

--- blueprints/profile/profile.py
+++ blueprints/profile/profile.py
@@ -17,9 +17,16 @@ def register_to_app(flask_app, bcrypt):
 			return redirect(url_for("login.login"))
 
 
-	@profile_bp.route('/profile/edit')
-	def edit_profile():
-		return redirect(url_for("register.index"))
+	@profile_bp.route('/profile/edit', methods=["GET", "POST"])
+	def edit():
+		if request.method == "POST":
+			current_user.full_name = request.form.get("full_name")
+			current_user.email = request.form.get("email")
+			current_user.bank_number = request.form.get("bank_number")
+			db.session.commit()
+			return redirect(url_for("profile.profile"))
+		else:
+			return render_template("edit_profile.html", full_name=str(current_user.full_name), email=str(current_user.email), bank_number=str(current_user.bank_number))
 
 
 	@profile_bp.route('/profile/logout')
